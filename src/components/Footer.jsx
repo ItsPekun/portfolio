52916a4248032414e4e4878df66c1461bde324a4
Pedro Zambrano <espedrozambrano@gmail.com>
2025-10-13 17:12:06 -0400

    Header y Footer arreglados

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,19 +2,19 @@ import { Youtube, Instagram } from "lucide-react";
 
 function Footer() {
   return (
-    <footer className="w-full bg-gray-900/80 border-gray-700 backdrop-blur-sm">
-      <div className="container mx-auto px-6 py-4 flex justify-center space-x-10">
+    <footer className="w-screen bg-gray-900/80 border-gray-700 backdrop-blur-sm relative left-1/2 right-1/2 -mx-[50vw]">
+      <div className="container mx-auto px-6 py-4 flex flex-col md:flex-row justify-center space-x-10">
         <span className="text-gray-300 p-2">
           © 2025 todos los derechos reservados
         </span>
         <span className="text-gray-300">
           Sígueme en redes:
           <div className="flex justify-center gap-4">
-            <a href="https://www.youtube.com/@pekundev2004">
-              <Youtube className=""/>
+            <a href="https://www.youtube.com/@pekundev2004" target="_blank">
+              <Youtube className="transform hover:-translate-y-1 hover:scale-110 transition-all duration-300" />
             </a>
-            <a href="https://www.instagram.com/itspekunyt/">
-               <Instagram />
+            <a href="https://www.instagram.com/itspekunyt/" target="_blank">
+              <Instagram className="transform hover:-translate-y-1 hover:scale-110 transition-all duration-300" />
             </a>
           </div>
         </span>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,7 +11,7 @@ function Header() {
   return (
     <header className="fixed top-0 left-0 w-full bg-gray-900/80 backdrop-blur-sm z-50 border-b border-gray-700">
       <nav className="container mx-auto px-6 py-4">
-        <div className="flex justify-between items-center">
+        <div className="flex flex-col sm:flex-row justify-between items-center">
           <Link
             to="hero"
             {...scrollSettings}
@@ -20,7 +20,7 @@ function Header() {
             Pedro Zambrano
           </Link>
 
-          <div className="flex space-x-8">
+          <div className="flex flex-col sm:flex-row sm:justify-start space-x-8">
             <Link
               to="about"
               {...scrollSettings}
