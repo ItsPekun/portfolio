a2c61ec29a8a44144682d0f8e01482599ab4a09b
Pedro Zambrano <espedrozambrano@gmail.com>
2026-07-17 20:11:16 -0400

    Actualizaciones de perfil

--- src/data/skillsData.js
+++ src/data/skillsData.js
@@ -1,23 +1,30 @@
 export const skillsData = [
-    {
-      title: "Frontend",
-      skills: ["React", "Next.js", "Tailwind CSS", "JavaScript"],
-    },
-    {
-      title: "Backend",
-      skills: ["Node.js", "Express", "Django", "Flask", "Laravel"],
-    },
-    {
-      title: "Bases de Datos",
-      skills: ["MongoDB", "MySQL", "PostgreSQL", "SQLite3"],
-    },
-    {
-      title: "Herramientas",
-      skills: ["Git", "Github"],
-    },
-    {
-      title: "Otras habilidades",
-      skills: ["Redes básicas", "Ciberseguridad"],
-    },
-    
-  ];
+  {
+    title: "Frontend",
+    skills: ["React", "Next.js", "Tailwind CSS", "JavaScript"],
+  },
+  {
+    title: "Backend",
+    skills: ["Node.js", "Express", "Django", "Flask", "Laravel"],
+  },
+  {
+    title: "Bases de Datos",
+    skills: ["MongoDB", "MySQL", "PostgreSQL", "SQLite3"],
+  },
+  {
+    title: "Herramientas",
+    skills: ["Git", "Github", "Docker"],
+  },
+  {
+    title: "Linux",
+    skills: [
+      "Administración avanzada",
+      "Optimización de entornos",
+      "Automatización de procesos",
+    ],
+  },
+  {
+    title: "Otras habilidades",
+    skills: ["Redes básicas", "Ciberseguridad"],
+  },
+];
